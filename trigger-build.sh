67b19b04a83b4cc2367a047385d85dea352a9411
Nabil Md Shahid <nabilmd901@gmail.com>
2025-07-30 04:23:42 +0600

    Sidecar/react (#1)
    * add node image
    
    * add tools
    
    * update tool name
    
    * update Jenkinsfile
    
    * update Jenkinsfile
    
    * add build trigger for react

--- trigger-build.sh
+++ trigger-build.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-JENKINS_URL="http://localhost:8888"
-USERNAME="admin"
-API_TOKEN="11e342d9c1abfc4af2289571c4da7d4e93"
-JOB_NAME="sidecar-react"
+set -a
+source .env
+set +a
 
-# Get crumb
+# ======================REACT========================= #
 CRUMB=$(curl -s --user "$USERNAME:$API_TOKEN" \
   "$JENKINS_URL/crumbIssuer/api/json" | jq -r '.crumb')
-
-# Trigger job with crumb
-curl -X POST "$JENKINS_URL/job/$JOB_NAME/build" \
+curl -X POST "$JENKINS_URL/job/$REACT_JOB_NAME/build" \
   --user "$USERNAME:$API_TOKEN" \
   -H "Jenkins-Crumb: $CRUMB"
+# ======================REACT========================= #
